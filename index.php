a866fa71a3d2f30c8b5ddcc83ee847a6e229aa14
Carlos <carlosparda7@gmail.com>
2015-10-25 20:36:06 -0600

    Agregación de base de datos
    Se agrega la base de datos así como la documentación de la misma

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<title>Inicio </title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+	<div class="container">
+	<div class="row">
+	<div class="col-md-12">
+			<h2>Bienvenido a <span class="lead"><img src="../../../Users/CARLOS/Desktop/Codeando/logo.png" width="309" height="75" alt=""/></span></h2>
+			<p class="lead">&nbsp;</p>
+			<p>Para entrar a la pagina</p>
+			<p>Instrucciones:</p>
+			<ol>
+				<li>Si eres nuevo en el sitio registrate en la opcion de registro.</li>
+				<li>Inicie sesion en la opcion de login.</li>
+				<li>Para finalizar la sesion, click en la opcion salir .</li>
+			</ol>
+			<br>
+</div>
+	</div>
+	</div>
+	</body>
+</html>
